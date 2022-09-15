890b1923307f5a9d960061f596294202f7e085dc
Nataliiv <nataliya1986kaduk@gmail.com>
2022-09-15 15:22:02 +0300

    Added react-loader-spinner

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React from "react";
+import { Audio } from "react-loader-spinner";
 
 export default function Weather(props) {
     function showWeather(response) {
@@ -8,5 +9,15 @@ export default function Weather(props) {
     let apiKey = "3743a596ca777c1b75d0b29a0dd4cdfd";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(showWeather);
-    return <h2>Hello World</h2>
+    return (
+      <Audio
+        height="80"
+        width="80"
+        radius="9"
+        color="green"
+        ariaLabel="loading"
+        wrapperStyle
+        wrapperClass
+      />
+    );
 }
